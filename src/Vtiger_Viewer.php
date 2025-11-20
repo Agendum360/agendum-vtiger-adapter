186cdd0d70ecca1bb2dc59d6c17bc1a354f4d2f9
francesco <info.francescobianco@gmail.com>
2025-11-20 17:36:27 +0100

    Updated at gio 20 nov 2025, 17:36:27, CET

--- src/Vtiger_Viewer.php
+++ src/Vtiger_Viewer.php
@@ -1,20 +1,10 @@
 <?php
 
-// Mock dependencies only when testing this specific adapter library
-if (!class_exists('SmartyBC')) {
-    if (getenv('VTIGER_ADAPTER_TESTING') === 'true' &&
-        basename(dirname(__DIR__)) === 'agendum-vtiger-adapter') {
-        class SmartyBC {}
-    } else {
-        require_once 'vendor/smarty/smarty/libs/SmartyBC.class.php';
-    }
-}
-
 // Mock Agendum_Viewer only when testing this specific adapter library
 if (!class_exists('Agendum_Viewer')) {
     if (getenv('VTIGER_ADAPTER_TESTING') === 'true' &&
         basename(dirname(__DIR__)) === 'agendum-vtiger-adapter') {
-        class Agendum_Viewer extends SmartyBC {}
+        class Agendum_Viewer {}
     } else {
         require_once 'includes/runtime/Viewer.php';
     }
